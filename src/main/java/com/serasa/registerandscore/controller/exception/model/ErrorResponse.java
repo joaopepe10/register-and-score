deb70488837a219ba8f2aa860d0d5986ab6e3d36
JOÃO P BARÇANTE <joaopedropires61@gmail.com>
2026-05-21 21:36:04 -0300

    feat: handle error

---
+++ src/main/java/com/serasa/registerandscore/controller/exception/model/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.serasa.registerandscore.controller.exception.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import org.springframework.http.HttpStatus;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public record ErrorResponse(
+        HttpStatus status,
+        ErrorCode code,
+        String error,
+        String detail
+) {
+}
