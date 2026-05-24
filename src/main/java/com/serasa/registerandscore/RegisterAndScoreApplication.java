b167abff577c7b527e87a26110f688db9b22ca99
JOÃO P BARÇANTE <joaopedropires61@gmail.com>
2026-05-24 16:40:01 -0300

    feat: register user

--- src/main/java/com/serasa/registerandscore/RegisterAndScoreApplication.java
+++ src/main/java/com/serasa/registerandscore/RegisterAndScoreApplication.java
@@ -2,8 +2,10 @@ package com.serasa.registerandscore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RegisterAndScoreApplication {
 
     public static void main(String[] args) {
